18ff2ca853f036df83efaccf4db206c016d9ee18
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2024-07-05 11:29:24 +0400

    Fixed Fruit bonanza
    Fixed the colliding issue with with fruit bonanza

---
+++ Project/Assets/Jeff's Stuff/Prefabs/plchldr script/FallingObjectController.cs
@@ -0,0 +1,51 @@
+
+using UnityEngine;
+
+enum FallingObjectType 
+{
+    Fruit=0,
+    Spider=1
+}
+/// <summary>
+/// This script is dedicated for the fruit
+/// </summary>
+public class FallingObjectController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField]FallingObjectType type;
+    GameController gameController;
+    [SerializeField] private float speed=-10;
+
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0f, speed);
+        if (transform.position.y < -7f) Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Basket")
+        {
+            if (type == FallingObjectType.Fruit)
+            {
+                Debug.Log("YOU GOT A FRUIT");
+                gameController.AddScore(1);
+                Destroy(this.gameObject);
+            }
+            else if(type == FallingObjectType.Spider)
+            {
+                Debug.Log("YOU GOT A SPIDER!");
+                gameController.DeductScore(1); // Deducts 1 point from the score
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
+
--- Project/Assets/Jeff's Stuff/Prefabs/plchldr script/spiderScript.cs
+++ Project/Assets/Jeff's Stuff/Prefabs/plchldr script/spiderScript.cs
@@ -1,33 +1,3 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-/// <summary>
-/// This script is dedicated for the spider
-/// </summary>
-public class spiderScript : MonoBehaviour
-{
-    Transform tr;
-    GameController gameController;
 
-    void Start()
-    {
-        tr = GetComponent<Transform>();
-        gameController = FindObjectOfType<GameController>();
-    }
-
-    void FixedUpdate()
-    {
-        tr.position -= new Vector3(0f, 0.12f, 0f);
-        if (tr.position.y < -7f) Destroy(this.gameObject);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Basket1")
-        {
-            Debug.Log("YOU GOT A SPIDER!");
-            gameController.DeductScore(1); // Deducts 1 point from the score
-            Destroy(this.gameObject);
-        }
-    }
-}
