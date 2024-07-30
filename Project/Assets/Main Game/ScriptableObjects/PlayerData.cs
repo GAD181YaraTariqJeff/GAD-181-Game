27d83025c29dc9969d1b86547968f01df8b5c063
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2024-07-31 03:10:37 +0400

    Removed my test scripts

---
+++ Project/Assets/Main Game/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
+public class PlayerData : MonoBehaviour
+{
+    public Vector3 playerPosition;
+    public string currentScene;
+}
